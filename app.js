d44315ca9d006302e3099636633eb8497136fce5
Helton Carlos <heltoncarlospa@hotmail.com>
2022-02-02 23:41:35 -0300

    funcao

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ function fecharModal(){
     var modal = document.getElementById('abrir')
     modal.style.display = "none"
 }
+
+function clickEsquerda(){
+    alert("<")
+}
+
+function clickDireita(){
+    alert(">")
+}
