75ecceaed64a192ee2486ed7b16ff3532ea23efb
Helton Carlos <heltoncarlospa@hotmail.com>
2022-02-07 23:27:38 -0300

    slide fotos

--- app.js
+++ app.js
@@ -15,3 +15,20 @@ function clickEsquerda(){
 function clickDireita(){
     alert(">")
 }
+
+
+function abrirEtapaUm(){
+    document.getElementById('etapa-um').style.display = 'flex'
+    document.getElementById('etapa-dois').style.display = 'none'
+    document.getElementById('circulo-um').style. backgroundColor = '#4e4e4e'
+    document.getElementById('circulo-dois').style. backgroundColor = '#cccccc'
+
+}
+
+
+function abrirEtapaDois(){
+    document.getElementById('etapa-um').style.display = 'none'
+    document.getElementById('etapa-dois').style.display = 'flex'
+    document.getElementById('circulo-dois').style. backgroundColor = '#4e4e4e'
+    document.getElementById('circulo-um').style. backgroundColor = '#cccccc'
+}
