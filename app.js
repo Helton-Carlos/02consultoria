7a722175110608c222f0deb6713223063d69f1c6
Helton Carlos <heltoncarlospa@hotmail.com>
2022-02-13 22:28:14 -0300

    slide responsivo

--- app.js
+++ app.js
@@ -25,10 +25,42 @@ function abrirEtapaUm(){
 
 }
 
-
 function abrirEtapaDois(){
     document.getElementById('etapa-um').style.display = 'none'
     document.getElementById('etapa-dois').style.display = 'flex'
     document.getElementById('circulo-dois').style. backgroundColor = '#4e4e4e'
     document.getElementById('circulo-um').style. backgroundColor = '#cccccc'
+}
+
+var slider_content = document.getElementById('box');
+
+// contain images in an array
+var image = ['2', '3', '4','5','6','7', '8', '9','10','11','12', '13', '14','15','16','17', '18', '19','20','1'];
+
+var i = image.length;
+
+
+// function for next slide 
+
+function clickDireita(){
+  if (i<image.length) {
+      i= i+1;
+  }else{
+      i = 1;
+  }
+    slider_content.innerHTML = "<img src='imagens/"+image[i-1]+".png'>";
+}
+
+
+// function for prew slide
+
+function clickEsquerda(){
+
+  if (i<image.length+1 && i>1) {
+      i= i-1;
+  }else{
+      i = image.length;
+  }
+    slider_content.innerHTML =  "<img src='imagens/"+image[i-1]+".png'>";
+
 }
